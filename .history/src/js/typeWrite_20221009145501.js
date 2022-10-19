d5a42be51a66417d6868adc7d976e0ad2d59a4d6
Vijayasenthil E <97862544+iamevs@users.noreply.github.com>
2022-10-19 22:49:57 +0530

    Delete .history directory

--- .history/src/js/typeWrite_20221009145501.js
+++
@@ -1,9 +0,0 @@
-export function typeWrite(elemento) {
-    const textoArray = elemento.innerHTML.split("");
-    elemento.innerHTML = " ";
-    textoArray.forEach(function (letra, i) {
-      setTimeout(function () {
-        elemento.innerHTML += letra;
-      }, 100 * i);
-    });
-  }
