8475c29ca60a4ff4338aa62c2caf4b51e707d575
iamevs <iamevs143@gmail.com>
2022-11-04 16:40:49 +0530

    dropdown js updated

---
+++ .history/src/js/main_20221104163724.js
@@ -0,0 +1,106 @@
+import { menu } from "./menu.js";
+
+menu();
+
+// about-text animation
+
+const text = document.querySelector(".text");
+text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
+
+const element = document.querySelectorAll("span");
+for (let i = 0; i < element.length; i++) {
+  element[i].addEventListener("mouseover", function () {
+    this.classList.add("active-text");
+  });
+}
+
+
+// navbar animation
+
+window.addEventListener("scroll", function () {
+  const header = document.querySelector("header");
+  header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+let nav = document.querySelector("nav");
+document.querySelector(".toggle").onclick = function () {
+  this.classList.toggle("active");
+  nav.classList.toggle("active");
+}
+nav.onclick = function () {
+  this.classList.remove("active");
+  document.querySelector(".toggle").classList.remove("active");
+}
+
+let filename = [
+  'HTML', 'CSS', 'JAVASCRIPT',
+  'Bootstrap', 'Tailwind', 'REACT',
+  'PYTHON', 'SASS',
+  'C/C++', 'NODEJS', 'MONGODB',
+  'EXPRESS', 'MYSQL', 'JQUERY', 'JAVA', 'Svelte', 'Astro', 'Next js', 'Three js', 'D3 js', 'PHP', 'NPM'
+];
+
+var tagCloud = TagCloud('.Sphere', filename, {
+
+  radius: 250,
+  maxSpeed: 'fast',
+  initSpeed: 'fast',
+  direction: 135,
+  keep: true
+
+});
+
+var color = '#3a86ff';
+document.querySelector('.Sphere').style.color = color;
+
+
+document.onclick = hideMenu;
+document.oncontextmenu = rightClick;
+
+function hideMenu() {
+  document.getElementById(
+    "contextMenu").style.display = "none"
+}
+
+function rightClick(e) {
+  e.preventDefault();
+
+  if (document.getElementById(
+    "contextMenu").style.display == "block")
+    hideMenu();
+  else {
+    var menu = document
+      .getElementById("contextMenu")
+
+    menu.style.display = 'block';
+    menu.style.left = e.pageX + "px";
+    menu.style.top = e.pageY + "px";
+    console.log(e.pageX, e.pageY);
+  }
+}
+
+
+//skill section
+function down(x, y) {
+  x.addEventListener("mouseover", function () {
+    y.style.display = "block";
+    x.classList.add("enable");
+  });
+}
+
+const adown = document.querySelector('.a');
+const content = document.querySelector(".a1");
+down(adown, content);
+// adown.addEventListener("mouseover", () => {
+//   content.style.display = "block";
+//   adown.classList.add("enable");
+// });
+document.addEventListener("click", (e) => {
+  if (e.target !== adown && e.target !== content) {
+    content.style.display = "none";
+    adown.classList.remove("enable");
+  }
+});
+
+const jsdown = document.querySelector('.js');
+const jscontent = document.querySelector(".js-lib");
