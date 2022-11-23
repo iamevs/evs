75bf8476d5e6c2b587fca2db5f7a07b7277b3d52
iamevs <iamevs143@gmail.com>
2022-11-23 23:29:54 +0530

    Scroll Bar updated

---
+++ .history/src/components/custom-context_20221123231354.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function CustomContext() {
+    return (
+        <div id="contextMenu" class="context-menu">
+            <ul>
+                <li><a href="/">Element-a</a></li>
+                <li><a href="/">Element-b</a></li>
+            </ul>
+        </div>
+    )
+}
+
+export default CustomContext;
